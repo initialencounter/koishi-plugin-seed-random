66f975b4db71a0748e1d10311bb017341b00e5ed
initialencounter <109729945+initialencounter@users.noreply.github.com>
2023-01-13 23:47:03 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,7 @@ export function setRandomNumber(num:number) {
   }
 }
 export function apply(ctx: Context) {
+  ctx.i18n.define('zh', require('./locales/zh'))
   ctx.command('jrrp')
     .alias('今日人品')
     .action(({ session }) => {
